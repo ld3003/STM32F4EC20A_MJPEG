c8555a0f956ce71691b4722d7701c0fa2611758c
liu <liu>
2018-07-20 17:45:58 +0800

    init

---
+++ Project/STM32F4xx_StdPeriph_Templates/main.c
@@ -0,0 +1,238 @@
+/**
+  ******************************************************************************
+  * @file    Project/STM32F4xx_StdPeriph_Templates/main.c 
+  * @author  MCD Application Team
+  * @version V1.6.1
+  * @date    21-October-2015
+  * @brief   Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT 2015 STMicroelectronics</center></h2>
+  *
+  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+  * You may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at:
+  *
+  *        http://www.st.com/software_license_agreement_liberty_v2
+  *
+  * Unless required by applicable law or agreed to in writing, software 
+  * distributed under the License is distributed on an "AS IS" BASIS, 
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/** @addtogroup Template_Project
+  * @{
+  */ 
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+static __IO uint32_t uwTimingDelay;
+RCC_ClocksTypeDef RCC_Clocks;
+
+/* Private function prototypes -----------------------------------------------*/
+static void Delay(__IO uint32_t nTime);
+
+/* Private functions ---------------------------------------------------------*/
+
+
+void USART2_Configuration(void)//
+{
+    GPIO_InitTypeDef  GPIO_InitStructure;
+    USART_InitTypeDef USART_InitStructure;
+		NVIC_InitTypeDef NVIC_InitStructure;
+	
+	
+	
+		RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2,ENABLE);
+		RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA,ENABLE);
+	
+		
+		/* Connect PXx to USARTx_Tx*/
+		GPIO_PinAFConfig(GPIOA, GPIO_PinSource2, GPIO_AF_USART2);
+
+		/* Connect PXx to USARTx_Rx*/
+		GPIO_PinAFConfig(GPIOA, GPIO_PinSource3, GPIO_AF_USART2);
+     
+//    /*
+//    *  USART2_TX -> PA2 , USART2_RX ->  PA3
+//    */                                
+//    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2|GPIO_Pin_3;                 
+//    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+//    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+//    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+//    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+//    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+
+		/* Configure USART Tx as alternate function  */
+		GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+		GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+		GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+
+		GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
+		GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+		GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+		/* Configure USART Rx as alternate function  */
+		GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+		GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
+		GPIO_Init(GPIOA, &GPIO_InitStructure);
+   
+    USART_InitStructure.USART_BaudRate = 115200;//יט??'??ת2¨ל??ך
+    USART_InitStructure.USART_WordLength = USART_WordLength_8b;//יט??ךy?Y??
+    USART_InitStructure.USART_StopBits = USART_StopBits_1;//יט??ם??1??
+    USART_InitStructure.USART_Parity = USART_Parity_No;//יט??D§?י??
+    USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;//יט??בק????
+    USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;//יט??1₪׳ק??ך?
+    USART_Init(USART2, &USART_InitStructure); //????ט??ב11ל?
+
+ 
+   
+    USART_Cmd(USART2, ENABLE);//ך1?ü'??ת1
+}
+
+void UART2_send_byte(unsigned char byte)
+{
+    USART_SendData(USART2, (uint8_t) byte);
+
+		/* Loop until transmit data register is empty */
+		while (USART_GetFlagStatus(USART2, USART_FLAG_TXE) == RESET)
+		{} 
+}
+
+
+/**
+  * @brief  Main program
+  * @param  None
+  * @retval None
+  */
+int main(void)
+{
+  GPIO_InitTypeDef GPIO_InitStructure;
+ 
+  /*!< At this stage the microcontroller clock setting is already configured, 
+       this is done through SystemInit() function which is called from startup
+       files before to branch to application main.
+       To reconfigure the default setting of SystemInit() function, 
+       refer to system_stm32f4xx.c file */
+
+  /* SysTick end of count event each 10ms */
+  RCC_GetClocksFreq(&RCC_Clocks);
+  SysTick_Config(RCC_Clocks.HCLK_Frequency / 100);
+	USART2_Configuration();
+	for(;;)
+	{
+		
+		UART2_send_byte('A');
+		UART2_send_byte('T');
+		UART2_send_byte('\r');
+		UART2_send_byte('\n');
+		
+		Delay(20);
+	}
+	
+  
+  /* Add your application code here */
+  /* Insert 50 ms delay */
+  Delay(200);
+  
+  /* Output HSE clock on MCO1 pin(PA8) ****************************************/ 
+  /* Enable the GPIOA peripheral */ 
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+  
+  /* Configure MCO1 pin(PA8) in alternate function */
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;  
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+    
+  /* HSE clock selected to output on MCO1 pin(PA8)*/
+  RCC_MCO1Config(RCC_MCO1Source_HSE, RCC_MCO1Div_1);
+  
+  
+  /* Output SYSCLK/4 clock on MCO2 pin(PC9) ***********************************/ 
+  /* Enable the GPIOACperipheral */ 
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
+  
+  /* Configure MCO2 pin(PC9) in alternate function */
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;  
+  GPIO_Init(GPIOC, &GPIO_InitStructure);
+    
+  /* SYSCLK/4 clock selected to output on MCO2 pin(PC9)*/
+  RCC_MCO2Config(RCC_MCO2Source_SYSCLK, RCC_MCO2Div_4);
+  
+     
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief  Inserts a delay time.
+  * @param  nTime: specifies the delay time length, in milliseconds.
+  * @retval None
+  */
+void Delay(__IO uint32_t nTime)
+{ 
+  uwTimingDelay = nTime;
+
+  while(uwTimingDelay != 0);
+}
+
+/**
+  * @brief  Decrements the TimingDelay variable.
+  * @param  None
+  * @retval None
+  */
+void TimingDelay_Decrement(void)
+{
+  if (uwTimingDelay != 0x00)
+  { 
+    uwTimingDelay--;
+  }
+}
+
+#ifdef  USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{ 
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
+
+/**
+  * @}
+  */
+
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
